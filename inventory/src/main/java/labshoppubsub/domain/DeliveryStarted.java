f6337f8ec3d3c6cf4feb87f641416799609bc782
Jinyoung Jang <jyjang@uengine.org>
2023-02-16 14:35:38 +0900

    commit

---
+++ inventory/src/main/java/labshoppubsub/domain/DeliveryStarted.java
@@ -0,0 +1,15 @@
+package labshoppubsub.domain;
+
+import labshoppubsub.domain.*;
+import labshoppubsub.infra.AbstractEvent;
+import lombok.*;
+import java.util.*;
+@Data
+@ToString
+public class DeliveryStarted extends AbstractEvent {
+
+    private Long id;
+    private Long orderId;
+}
+
+
